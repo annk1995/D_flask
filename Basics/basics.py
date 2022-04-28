004cbee19015e66c0f6b67596b97e294740f4610
annk1995 <kabuikariuki052@gmail.com>
2022-04-28 08:47:48 +0300

    added to basics

--- Basics/basics.py
+++ Basics/basics.py
@@ -1,15 +1,20 @@
+
 from flask import Flask
 from daily import *
 
 app=Flask(__name__)
+
 @app.route('/')
 def intro():
     #home.html
     return get_all_to_do()
-@app.route("/about")
-def about():
-    return "<h1>about my flask app</h1>"
 
+app.route('/day/<name>')
+
+
+def day(name):
+    print(name)
+    return get_the_to_do(name)
 
 if __name__=="_main_":
     app.run(debug=True)
--- Basics/daily.py
+++ Basics/daily.py
@@ -1,21 +1,30 @@
 
-toDo={
-     "monday":"play football",
-      "Tuesday":"dance",
-       "wednesday":"skip",
-        "Thursday":"bake",
-         "friday":"out",
-          "saturday":"dance",
-          "sunday":"eat chicken"
- }
-def get_all_to_do():
-     return f"<ul>{to_do_do()}</ul>"
-def to_do_do():
-    li=""
-    for key in toDo:
-        li=li+f"<li><h1>on: <b>{key}</b> we {toDo[key]}</h1></li>"
-        return li
 
-        
+toDo={
+    "monday":"Play Football",
+    "tuesday":"CODE",
+    "wendesday":"Go To The Gym",
+    "thursday":"Bake a cake",
+    "friday":"Attend a dance class",
+    "saturday":"Go To church",
+    "sunday":"Movie Time"
+}
+
+def get_all_to_do():
+    return f"<ul>{to_to_do()}</ul>"
+
+def to_to_do():
+    li=""
+    # for key in toDo:
+    #     li=li+f"<li><h1>On:<b>{key}</b> we {toDo[key]}</h1></li>"
+    for key in toDo:
+        href=f"http://127.0.0.1:5000/day/{key}"
+        li=li+f"<a href={href}><li><h1 >On:<b>{key}</b> we {toDo[key]}</h1></li></a>"
+    return li
+
+def get_the_to_do(key):
+    return f"<h1>On This Day {key}</h1><h2>{toDo[key]}</h2>"
+
+      
 
     
